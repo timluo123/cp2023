32f6f8fa094a8919c5bc2e5451b577de62a7b145
timluo123 <145647459+timluo123@users.noreply.github.com>
2023-12-27 13:34:18 +0000

    新增w15

---
+++ downloads/3.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() 
+{
+    // Print a line of hashes
+    printf("######\n");
+
+    // Print a single hash
+    printf("#\n");
+
+    // Print a single hash
+    printf("#\n");
+
+    // Print a line of hashes
+    printf("#####\n");
+
+    // Print a single hash
+    printf("#\n");
+
+    // Print a single hash
+    printf("#\n");
+
+    // Print a single hash
+    printf("#\n");
+
+    return(0);
+}
+#include <stdio.h>
+
+int main() 
+{
+    // Print top line of pattern
+    printf("    ######\n");
+
+    // Print second line of pattern
+    printf("  ##      ##\n");
+
+    // Print lines 3 to 7 of pattern
+    printf(" #\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf(" #\n");
+    printf(" #\n");
+
+    // Print bottom line of pattern
+    printf("  ##      ##\n");
+
+    // Print last line of pattern
+    printf("    ######\n");
+
+    return(0);
+}
